9afc672596183f08d22df407f7a7246e3c0cb4a5
berithx <seere7777@gmail.com>
2024-05-03 16:32:02 +0900

    first commit for Main class

--- src/score/Score.java
+++ src/score/Score.java
@@ -1,5 +1,15 @@
 package score;
 
+import java.util.ArrayList;
+
 public class Score {
-    테스트
+    int studentId;  // 수강생 고유번호
+    int subjectId;  // 과목 고유번호
+    ArrayList<Integer> roundAndScore;
+    ArrayList<String> grade;
+
+    public Score() {
+        roundAndScore = new ArrayList<>();
+        grade = new ArrayList<>();
+    }
 }
