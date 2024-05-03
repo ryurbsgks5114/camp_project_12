f8d8cf08628b144d4174877044a3335ae055ee76
marlboro09 <taebong8989@gmail.com>
2024-05-03 16:02:05 +0900

    3개의 변수 추가

--- src/Subject.java
+++ src/Subject.java
@@ -1,5 +1,22 @@
 public class Subject {
-    public static void main(String[] args) {
-        System.out.println("몰?루");
-    }
+    private int subjectId;
+    private String subjectName;
+    private int subjectType;
+
+    public Subject(int subjectId, String subjectName, int subjectType)
+        this.subjectId = subjectId;
+        this.subjectName = subjectName;
+        this.subjectType = subjectType;
+}
+
+public int getSubjectId() {
+    return subjectId;
+}
+
+public String getSubjectName() {
+    return subjectName;
+}
+
+public int getSubjectType() {
+    return subjectType;
 }
