f70da14859c897555ab9f444645bd21a268bf3af
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-05-03 16:50:19 +0900

    commit for pull

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static final String SUBJECT_TYPE_MANDATORY = "필수";
+    private static final String SUBJECT_TYPE_CHOICE = "선택";
+    private static int studentIdx;
+    private static int subjectIdx;
+    private static int scoreIdx;
+
+    public Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("초기 상태 ");
         System.out.println("테스트");
     }
+
+    private static void setDataList() {
+
+//        List<Student> studentList = new ArrayList<>();
+//        List<Subject> subjectList = new ArrayList<>();
+//        List<Score> scoreList = new ArrayList<>();
+
+        List<String> mandatoryList = new ArrayList<String>();
+        List<String> choiceList = new ArrayList<String>();
+
+    }
+
 }
---
+++ src/list/List.java
@@ -0,0 +1,7 @@
+package list;
+
+public interface List<T> {
+
+
+
+}
